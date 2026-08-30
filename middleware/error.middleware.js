2203bdcdf25540e5bc106cd1dc1c66b093034356
Puja mangalpalli <pmsm901@gmail.com>
2026-08-30 15:13:37 +0530

    adding error middleware

---
+++ middleware/error.middleware.js
@@ -0,0 +1,9 @@
+const errorMiddleware =(err,req,res,next)=>{
+    console.log(err);
+    const statusCode=res.statusCode === 200 ? 500 :res.statusCode;
+    res.status(statusCode).json({
+        success:false,
+        message:err.message || "Internal Server Error" 
+    });
+};
+export default errorMiddleware;
