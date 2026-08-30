e2f732170e5bbac1e4384708f035c58bc75e63b5
Puja mangalpalli <pmsm901@gmail.com>
2026-08-30 13:02:05 +0530

    added video model

--- models/videos.model.js
+++ models/videos.model.js
@@ -1,1 +1,51 @@
 import mongoose from "mongoose";
+
+const videoSchema=mongoose.Schema(
+{
+        title:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        description:{
+            type:String,
+            default:""
+        },
+        videoUrl:{
+            type:String,
+            required:true
+        },
+        thumbnailUrl:{
+            type:String,
+            required:true
+        },
+        category:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Channel",
+            required:true
+        },
+        uploader:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        likes:{
+            type:Number,
+            default:0
+        },
+        dislikes:{
+            type:Number,
+            default:0
+        }
+
+
+},
+{
+    timestamps:true
+}
+);
+export default mongoose.model("Videos",videoSchema);
