9404491dff74cfd8ec107f08c5d6014ba0b047e9
Puja mangalpalli <pmsm901@gmail.com>
2026-08-31 18:18:18 +0530

    adding comment route

---
+++ routes/comment.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getComments,createComment,updateComment,deleteComment } from "../controllers/comment.controller";
+import protect from "../middleware/auth.middleware.js";
+
+const router=express.Router();
+
+//get comments
+router.get("/video/:videoId",getComments);
+//create comment
+router.post("/video/:videoId",protect,createComment);
+//update comment
+router.put("/:id",protect,updateComment);
+//delete comment
+router.delete("/:id",protect,deleteComment);
+
+export default router;
